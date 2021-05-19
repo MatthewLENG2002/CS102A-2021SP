a4f4e67bd506820eebf029c0584e3ec77adc9424
YZL24 <84270091+YZL24@users.noreply.github.com>
2021-05-19 23:28:00 +0800

    PlayerSwitch

---
+++ MineSweeper/src/cs102a/util/PlayerSwitch.java
@@ -0,0 +1,11 @@
+public class PlayerSwitch {
+
+    public PlayerSwitch(){
+        if ((Info.roundNow/Info.round)%2 ==0){
+            Info.playerNow=1;
+        }
+        else if ((Info.roundNow/Info.round)%2 ==1){
+            Info.playerNow=2;
+        }
+    }
+}
