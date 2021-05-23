cc3a29be7c35db9b47c6e74f9984eb1584d2c198
MatthewLENG2002 <77274680+MatthewLENG2002@users.noreply.github.com>
2021-05-23 21:00:50 +0800

    Amend HistoryLoader

--- MineSweeper/src/cs102a/gui/HistorySelector.java
+++ MineSweeper/src/cs102a/gui/HistorySelector.java
@@ -7,7 +7,6 @@ import java.awt.*;
 
 public class HistorySelector extends JFrame {
 
-    public static HistorySelector hs = new HistorySelector();
     public HistorySelector() {
         super("Mine Sweeper");
 
@@ -16,32 +15,54 @@ public class HistorySelector extends JFrame {
         title.setForeground(Color.BLACK);
         title.setOpaque(false);
 
-        JButton slot1 = new JButton("Slot1");
+        JButton slot1 = new JButton("Load History");
         slot1.setOpaque(false);
         slot1.setBorder(null);
         slot1.setForeground(Color.BLACK);
         slot1.setFont(new java.awt.Font("Times New Roman", Font.BOLD, 26));
         slot1.addActionListener(e -> {
+            try {
+                new HistoryLoader().load();
+                this.dispose();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+                JDialog loadFailure = new JDialog();
+                loadFailure.setVisible(true);
+                loadFailure.setDefaultCloseOperation(HIDE_ON_CLOSE);
+                loadFailure.setSize(120, 100);
+                loadFailure.setLocationRelativeTo(null);
+                JLabel failed = new JLabel("Load Failed!",JLabel.CENTER);
+                loadFailure.add(failed);
+            }
 
         });
 
-        JButton slot2 = new JButton("Slot2");
-        slot2.setOpaque(false);
-        slot2.setBorder(null);
-        slot2.setForeground(Color.BLACK);
-        slot2.setFont(new java.awt.Font("Times New Roman", Font.BOLD, 26));
-        slot2.addActionListener(e -> {
+        JButton LoadFile = new JButton("Load File");
+        LoadFile.setOpaque(false);
+        LoadFile.setBorder(null);
+        LoadFile.setForeground(Color.BLACK);
+        LoadFile.setFont(new java.awt.Font("Times New Roman", Font.BOLD, 26));
+        LoadFile.addActionListener(e -> {
+            JFileChooser loadFile = new JFileChooser("History");
+            loadFile.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            loadFile.showOpenDialog(null);
+            loadFile.setMultiSelectionEnabled(false);
+                try {
+                    new HistoryLoader().loadFromDirectoryFile(loadFile.getSelectedFile());
+                    this.dispose();
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                    JDialog loadFailure = new JDialog();
+                    loadFailure.setVisible(true);
+                    loadFailure.setDefaultCloseOperation(HIDE_ON_CLOSE);
+                    loadFailure.setSize(120, 100);
+                    loadFailure.setLocationRelativeTo(null);
+                    JLabel failed = new JLabel("Load Failed!",JLabel.CENTER);
+                    loadFailure.add(failed);
+                }
 
         });
 
-        JButton slot3 = new JButton("Slot3");
-        slot3.setOpaque(false);
-        slot3.setBorder(null);
-        slot3.setForeground(Color.BLACK);
-        slot3.setFont(new java.awt.Font("Times New Roman", Font.BOLD, 26));
-        slot3.addActionListener(e -> {
-
-        });
 
         JButton home = new JButton("Home");
         home.setOpaque(false);
@@ -49,14 +70,13 @@ public class HistorySelector extends JFrame {
         home.setForeground(Color.BLACK);
         home.setFont(new java.awt.Font("Times New Roman", Font.BOLD, 26));
         home.addActionListener(e -> {
-            hs.setVisible(false);
+            this.setVisible(false);
             StartMenu.start.setVisible(true);
         });
 
-        setLayout(new GridLayout(4, 1, 2, 2));
+        setLayout(new GridLayout(3, 1, 2, 2));
         add(new SmallGrid(3, 3, slot1));
-        add(new SmallGrid(3, 3, slot2));
-        add(new SmallGrid(3, 3, slot3));
+        add(new SmallGrid(3, 3, LoadFile));
         add(new SmallGrid(3, 3, home));
 
         this.setSize(500, 500);
