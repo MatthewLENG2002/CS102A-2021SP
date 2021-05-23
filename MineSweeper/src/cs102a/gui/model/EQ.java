69ab40cb8a62986e35c3827e73b80500f4f8acab
YZL24 <84270091+YZL24@users.noreply.github.com>
2021-05-23 20:37:15 +0800

    EQ

---
+++ MineSweeper/src/cs102a/gui/model/EQ.java
@@ -0,0 +1,35 @@
+import model.MusicPlayer;
+
+public class EQ {
+    public static EQ bgm1 = new EQ("bgm1",true);
+    public static EQ bgm2 = new EQ("bgm2",true);
+    public static EQ bomb = new EQ("bomb",false);
+    public static EQ flag = new EQ("flag",false);
+
+    MusicPlayer py;
+
+    public EQ(String fileName,boolean isLoop) {
+        py = new MusicPlayer("sound/" + fileName + ".wav");
+        py.setLoop(isLoop);
+    }
+
+    public final void play() {
+        py.setVolume(6);
+//        py.setLoop(isLoop);
+        py.play();
+
+        // ? consider its necessity
+//        if (!isLoop) System.gc();
+    }
+
+    public final void stop() {
+        py.over();
+    }
+
+    public final boolean changeIsMute() {
+        boolean nowIsMute=py.getVolume() == 6;
+        if (py.getVolume() == 6) py.setVolume(-8000);
+        else py.setVolume(6);
+        return nowIsMute;
+    }
+}
